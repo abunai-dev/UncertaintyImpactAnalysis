4b4d24f2d7cd96773358fbd43201fcb84f5d5e21
Alex | Kronox <alexander_vogt@outlook.de>
2024-04-02 11:06:43 +0200

    base frame

--- bundles/dev.abunai.impact.analysis/src/dev/abunai/impact/analysis/interactive/InteractiveAnalysisHandler.java
+++ bundles/dev.abunai.impact.analysis/src/dev/abunai/impact/analysis/interactive/InteractiveAnalysisHandler.java
@@ -1,27 +1,53 @@
 package dev.abunai.impact.analysis.interactive;
 
-import java.util.NoSuchElementException;
+import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class InteractiveAnalysisHandler {
 	
 	public void handle() {
-		int id = getID();
-		System.out.println("Input was: " + id);
+		System.out.println("Enter an id to check for:");
+		int id = getIntFromInput();
+		List<JsonUncertainty> uncertainties = getAllUncertainties();
+		JsonUncertainty uncertainty = uncertainties.stream().filter(u -> u.id() == id).findFirst().orElse(null);
+		if (uncertainty == null) {
+			String notFoundText = String.format("No uncertainty with id %i found.", id);
+			throw new IllegalArgumentException(notFoundText);
+		}
+		List<Object> allElements = getAllElements(ArchitecturalElementType.getFromName(uncertainty.architecturalElementType()));
+		Object elementToAnalize = selectElement(allElements);
 	}
 	
-	private int getID() {
-		System.out.println("Please enter an id to check for:");
+	private int getIntFromInput() {
 		int input = -1;
-		while (input < 0) {
-			try (Scanner scanner = new Scanner(System.in)) {
-				input = scanner.nextInt();
-			} catch (NoSuchElementException e) {
-				System.out.println("Please enter a valid id:");
-			}
+		try (Scanner scanner = new Scanner(System.in)) {
+			input = scanner.nextInt();
+		} catch (InputMismatchException e) {
+			throw new IllegalArgumentException(String.format("%s is not a valid number.", input));
 		}
-		
 		return input;
+	}
+	
+	private List<Object> getAllElements(ArchitecturalElementType element) {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+	
+	private List<JsonUncertainty> getAllUncertainties() {
+		throw new UnsupportedOperationException("Not yet implemented");
+	}
+	
+	private Object selectElement(List<Object> allElements) {
+		System.out.println("Select one of these element, by giving its line number:");
+		for (int i = 0; i < allElements.size(); i++) {
+			System.out.println(String.format("%d) %s", i + 1, allElements.get(i)));
+		}
+		int index = getIntFromInput();
+		if (index < 1 || index > allElements.size()) {
+			throw new IllegalArgumentException(String.format("Number %d is out of range.", index));
+		}
+		return allElements.get(index);
 	}
 	
 }
