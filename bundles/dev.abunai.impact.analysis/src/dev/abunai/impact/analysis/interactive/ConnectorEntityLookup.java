e6051ccb215f423b34821f92544a6b1e82ee38c2
Alex | Kronox <alexander_vogt@outlook.de>
2024-04-09 17:25:54 +0200

    fix connector lookup

--- bundles/dev.abunai.impact.analysis/src/dev/abunai/impact/analysis/interactive/ConnectorEntityLookup.java
+++ bundles/dev.abunai.impact.analysis/src/dev/abunai/impact/analysis/interactive/ConnectorEntityLookup.java
@@ -1,31 +1,42 @@
 package dev.abunai.impact.analysis.interactive;
 
 import java.util.List;
+import java.util.ArrayList;
 
+import org.palladiosimulator.pcm.core.composition.AssemblyConnector;
 import org.palladiosimulator.pcm.core.composition.CompositionPackage;
-import org.palladiosimulator.pcm.core.composition.Connector;
+import org.palladiosimulator.pcm.core.composition.ProvidedDelegationConnector;
 import org.palladiosimulator.pcm.core.entity.Entity;
 
 import dev.abunai.impact.analysis.PCMUncertaintyImpactAnalysis;
 
 public class ConnectorEntityLookup extends EntityLookup {
 
-	private final List<Connector> connectors;
+	private final List<AssemblyConnector> assemblyConnectors;
+	private final List<ProvidedDelegationConnector> providedDelegationConnector;
 	
 	public ConnectorEntityLookup(PCMUncertaintyImpactAnalysis analysis) {
 		super(analysis);
 		
-		connectors = findAllElementsOfType(CompositionPackage.eINSTANCE.getConnector(),Connector.class);
+		assemblyConnectors = findAllElementsOfType(CompositionPackage.eINSTANCE.getAssemblyConnector(), AssemblyConnector.class);
+		providedDelegationConnector = findAllElementsOfType(CompositionPackage.eINSTANCE.getProvidedDelegationConnector(), ProvidedDelegationConnector.class);
 	}
 
 	@Override
 	public List<Entity> getEntities() {
-		return connectors.stream().map(e -> (Entity)e).toList();
+		List<Entity> result = new ArrayList<>();
+		result.addAll(assemblyConnectors);
+		result.addAll(providedDelegationConnector);
+		return result;
 	}
 
 	@Override
 	public void addToAnalysis(int index) {
-		analysis.getUncertaintySources().addComponentUncertaintyInAssemblyContext(connectors.get(index).getId());
+		if (index < assemblyConnectors.size()) {
+			analysis.getUncertaintySources().addConnectorUncertaintyInConnector(assemblyConnectors.get(index).getId());
+		} else {
+			analysis.getUncertaintySources().addConnectorUncertaintyInConnector(providedDelegationConnector.get(index - assemblyConnectors.size()).getId());
+		}
 	}
 
 }
