94be9d8014e250ca30ecb03c3de3caee89b6c4c2
Alexander Vogt <alexander_vogt@outlook.de>
2024-12-15 14:13:10 +0100

    prohibit editing

---
+++ bundles/dev.abunai.impact.analysis/src/dev/abunai/impact/analysis/webview/viewer/src/editMode/hookOverwrites.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from "inversify";
+import {
+    CommandExecutionContext,
+    CommandReturn,
+    DeleteElementCommand,
+    EditLabelMouseListener,
+    SModelElementImpl
+} from "sprotty";
+import { Action } from "sprotty-protocol";
+
+@injectable()
+export class EditLabelMouseListenerRemove extends EditLabelMouseListener {
+    doubleClick(target: SModelElementImpl, event: MouseEvent): (Action | Promise<Action>)[] {
+        return []
+    }
+}
+
+@injectable()
+export class DeleteElementCommandRemove extends DeleteElementCommand {
+    execute(context: CommandExecutionContext): CommandReturn {
+        return context.root
+    }
+
+    undo(context: CommandExecutionContext): CommandReturn {
+        return context.root
+    }
+
+    redo(context: CommandExecutionContext): CommandReturn {
+        return context.root
+    }
+}
